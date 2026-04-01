6a51d443bf600f5435e8c81875e91856005e55c0
Zigosaurous <zactky143@gmail.com>
2026-04-01 11:02:24 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ practice_refinement/Guess_3_Digit.py
@@ -0,0 +1,37 @@
+import random
+secret_code = ""
+for i in range(3):
+    digit = str(random.randint(1, 5))
+    secret_code = secret_code + digit
+
+print(secret_code) # remember to comment off later  
+
+print("Guess the 3-digit number. Each digit is from 1 to 5: ")
+print("You have 5 tries. Enter your guess (e.g. 123). ")
+
+#ask the user to guess 
+
+for j in range(5):
+    guess = input("Enter your guess: ")
+
+    #count how many correct 
+
+    count = 0
+    for i in range(len(secret_code)):
+        if guess[i] == secret_code[i]:
+            count += 1
+    print(f"You have {count} correct digits in the correct position.")
+
+    # stop the user after 5 tries
+
+    if guess == secret_code:
+        print("You have guessed the code")
+        break
+    else:
+        print("You did not guess it")
+else:
+    # code here will run on successful completion of loop
+    print(f"The correct answer is {secret_code}")
+
+
+
