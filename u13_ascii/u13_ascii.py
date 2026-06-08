9d058fe524184b9ada9a60d6dd78bcee537d6486
Zigosaurous <zactky143@gmail.com>
2026-06-08 06:24:42 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ u13_ascii/u13_ascii.py
@@ -0,0 +1,15 @@
+#ord() = ordinal (number)      chr() = character
+
+print(ord("A"))
+print(ord("Z"))
+
+print(ord("a"))
+print(ord("z"))
+
+print(ord("0"))
+print(ord("9"))
+
+print(ord("!"))
+print(ord("-"))
+
+print(chr(65))
